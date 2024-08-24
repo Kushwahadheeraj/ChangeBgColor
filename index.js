8d5f2e5293b94d4937744cd2cac5fbccfa32fa98
Kushwahadheeraj <kushwahadheeraj245@gmail.com>
2024-08-24 14:50:19 +0530

    bgcolor

---
+++ index.js
@@ -0,0 +1,27 @@
+const randomCol=function(){
+    const hex="0123456789ABCDEF"
+    let color="#"
+    for(let i=0;i<6;i++){
+        color +=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+console.log(randomCol())
+
+let intervalId;
+const startChangingColor=function(){
+   intervalId= setInterval(changeBgColor,1000)
+
+    function changeBgColor(){
+        document.body.style.backgroundColor=randomCol();
+    }
+}
+const stopChangingColor=function(){
+    clearInterval(intervalId)  
+}
+
+document.querySelector('#start').addEventListener
+('click',startChangingColor);
+
+document.querySelector('#stop').addEventListener
+('click',stopChangingColor);
